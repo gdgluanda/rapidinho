f8c9792a1c0fc2b1ea68c70f920f81b9dc17fa5b
braulio94 <brauliocassule94@gmail.com>
2018-09-03 23:55:41 +0100

    add image asset

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -14,12 +14,6 @@ class _SplashPageState extends State<SplashPage> {
           children: [
             Center(
                 child: Image.asset('assets/images/rapidinho_transparent.png')),
-            Center(child: Text('Entregas na hora', style: TextStyle(
-                fontFamily: 'Rajdhani',
-                color: Colors.white,
-                fontSize: 17.0,
-                letterSpacing: 2.0,
-                fontWeight: FontWeight.w600))),
           ]
       ),
     );
