c3fba7c580e86a26b5893894925ad762039bf7d3
Pedro Massango <pedromassango.developer@gmail.com>
2018-09-04 11:11:12 +0100

    Added bottom navigation bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,10 +6,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
+
+  /// The Bottom Navigation Items
+  List<BottomNavigationBarItem> navigationItems() {
+    return [
+      BottomNavigationBarItem(
+        title: new Text("Home"),
+        icon: new Icon(Icons.home),
+        backgroundColor: Colors.redAccent,
+
+      ),
+      BottomNavigationBarItem(
+          title: new Text("Orders"),
+          icon: new Icon(Icons.reorder)
+      ),
+      BottomNavigationBarItem(
+          title: new Text("Cart"),
+          icon: new Icon(Icons.shopping_cart)
+      ),
+      BottomNavigationBarItem(
+          title: new Text('Account'),
+          icon: new Icon(Icons.account_circle),
+      )
+    ];
+  }
+
+  // selected index
+  int _currentIndex = 0;
+
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: new BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: navigationItems(),
+        onTap: (int index){
+          setState(() {
+            _currentIndex = index;
+          });
 
+        },
+      ),
     );
   }
 }
