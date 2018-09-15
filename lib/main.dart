823fc771ac743a111f52dd6fe5853b03fe4fe2bf
Braulio Cassule <brauliocassule94@gmail.com>
2018-09-15 10:24:54 +0100

    change status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rapidinho/pages/home_page.dart';
 import 'package:rapidinho/pages/splash_page.dart';
 
@@ -7,12 +8,20 @@ void main() => runApp(
 );
 
 class RapidinhoApp extends StatelessWidget {
+
+  RapidinhoApp(){
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.red[700], //or set color with: Color(0xFF0000FF)
+    ));
+  }
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Rapidinho',
       theme: ThemeData(
-        primarySwatch: Colors.red
+        primarySwatch: Colors.red,
+
       ),
       home: Stack(
         children: [
