024f360613339a6c6c4ff963feece91edfe42f9d
braulio94 <brauliocassule94@gmail.com>
2018-09-17 18:49:27 +0100

    remove text field and elliptical border

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -8,52 +8,13 @@ class HomeTab extends StatefulWidget {
 class _HomeTabState extends State<HomeTab> {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      margin: EdgeInsets.only(top: 55.0),
-      child: ListView(
-        children: [
-          Stack(
-            overflow: Overflow.visible,
-            children: <Widget>[
-              Container(
-                width: MediaQuery.of(context).size.width,
-                child: PhysicalModel(
-                  color: Colors.transparent,
-                  borderRadius:
-                      new BorderRadius.vertical(bottom: new Radius.elliptical(100.0, 20.0)),
-                  child: new Image.asset(
-                    'assets/images/featured_meal.jpg',
-                    height: 200.0,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              new Positioned(
-                bottom: -15.0,
-                width: MediaQuery.of(context).size.width,
-                child: new Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                  child: PhysicalModel(
-                    color: Colors.transparent,
-                    borderRadius: new BorderRadius.all(Radius.circular(50.0)),
-                    shadowColor: Colors.black,
-                    elevation: 10.0,
-                    child: Card(
-                      child: new Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "Pesquise aqui",
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ],
+    return Container(
+      alignment: Alignment.topCenter,
+      margin: EdgeInsets.only(top: kToolbarHeight),
+      child: Image.asset(
+        'assets/images/featured_meal.jpg',
+        fit: BoxFit.fill,
+        //height: 200.0,
       ),
     );
   }
