e2e555f84c54a77e1db3b635c61e36a20e7c8a26
pedromassango <pedromassango.developer@gmail.com>
2018-10-03 20:31:11 +0100

    account tab created

---
+++ lib/tabs/account_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountTab extends StatefulWidget{
+
+  @override
+  _AccountTabState createState() => _AccountTabState();
+}
+
+class _AccountTabState extends State<AccountTab>{
+
+  @override
+  Widget build(BuildContext buildContext){
+    return Center();
+  }
+}
