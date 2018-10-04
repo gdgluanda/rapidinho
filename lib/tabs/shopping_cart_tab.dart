003722bfb274f978a7c29509a01078d307d3d5ad
Braulio Cassule <brauliocassule94@gmail.com>
2018-10-04 02:47:22 +0100

    create shopping cart tab

---
+++ lib/tabs/shopping_cart_tab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCartTab extends StatefulWidget {
+  @override
+  _ShoppingCartTabState createState() => _ShoppingCartTabState();
+}
+
+class _ShoppingCartTabState extends State<ShoppingCartTab> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
