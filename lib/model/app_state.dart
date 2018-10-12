67853b2b6cb4b1b0ac9e61c8d7934232e7556a5e
Braulio Cassule <brauliocassule94@gmail.com>
2018-10-12 13:32:03 +0100

    create application redux state

---
+++ lib/model/app_state.dart
@@ -0,0 +1,35 @@
+
+import 'package:meta/meta.dart';
+import 'package:rapidinho/model/navigation_tabs.dart';
+
+@immutable
+class AppState {
+  final NavigationTab activeTab;
+
+  AppState({this.activeTab = NavigationTab.Home});
+
+  factory AppState.init() => AppState();
+
+  AppState copyWith({
+    NavigationTab activeTab,
+  }){
+    return AppState(
+      activeTab: activeTab ?? this.activeTab,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is AppState &&
+              runtimeType == other.runtimeType &&
+              activeTab == other.activeTab;
+
+  @override
+  int get hashCode => activeTab.hashCode;
+
+  @override
+  String toString() {
+    return 'AppState{activeTab: $activeTab}';
+  }
+}
