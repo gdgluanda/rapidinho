d896b9be15eed101ecf1071551c3cf8718d1e022
Braulio Cassule <brauliocassule94@gmail.com>
2018-11-27 11:44:44 +0100

    close backdrop on WillPopScope action

--- lib/ui/product/products_page.dart
+++ lib/ui/product/products_page.dart
@@ -68,43 +68,54 @@ class _ProductsPageState extends State<ProductsPage> with SingleTickerProviderSt
     super.dispose();
   }
 
+  Future<bool> _exitProductsPage() async {
+    if(_backdropPanelVisible){
+      _toggleBackdropPanelVisibility();
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('${widget.product.name}')),
-      body: LayoutBuilder(builder: (context, constraints) {
-        int i = 0;
-        return Stack(
-          children: <Widget>[
-            ProductGrid(onTap: _toggleBackdropPanelVisibility, productType: widget.product),
-            IgnorePointer(
-              ignoring: !_backdropPanelVisible,
-              child: GestureDetector(
-                onTap: _toggleBackdropPanelVisibility,
-                child: Container(
-                  color: Colors.transparent.withOpacity(_controller.value / 2),
+    return WillPopScope(
+      onWillPop: _exitProductsPage,
+      child: Scaffold(
+        appBar: AppBar(title: Text('${widget.product.name}')),
+        body: LayoutBuilder(builder: (context, constraints) {
+          return Stack(
+            children: <Widget>[
+              ProductGrid(onTap: _toggleBackdropPanelVisibility, productType: widget.product),
+              IgnorePointer(
+                ignoring: !_backdropPanelVisible,
+                child: GestureDetector(
+                  onTap: _toggleBackdropPanelVisibility,
+                  child: Container(
+                    color: Colors.transparent.withOpacity(_controller.value / 2),
+                  ),
                 ),
               ),
-            ),
-            BackdropPanel(
-              key: _globalKey,
-              color: Colors.white,
-              onTap: _toggleBackdropPanelVisibility,
-              onVerticalDragUpdate: _handleDragUpdate,
-              onVerticalDragEnd: _handleDragEnd,
-              controller: _controller,
-              constraints: constraints,
-              title: Text(
-                MockData.productList.firstWhere((Product product) => product.categoryId == widget.product.category.index).itemName,
-                style: RapidinhoTextStyle.largeText.copyWith(fontWeight: FontWeight.w600, color: Colors.black54),
+              BackdropPanel(
+                key: _globalKey,
+                color: Colors.white,
+                onTap: _toggleBackdropPanelVisibility,
+                onVerticalDragUpdate: _handleDragUpdate,
+                onVerticalDragEnd: _handleDragEnd,
+                controller: _controller,
+                constraints: constraints,
+                title: Text(
+                  MockData.productList.firstWhere((Product product) => product.categoryId == widget.product.category.index).itemName,
+                  style: RapidinhoTextStyle.largeText.copyWith(fontWeight: FontWeight.w600, color: Colors.black54),
+                ),
+                child: ProductCard.large(
+                  product: MockData.productList.firstWhere((Product product) => product.categoryId == widget.product.category.index),
+                ),
               ),
-              child: ProductCard.large(
-                product: MockData.productList.firstWhere((Product product) => product.categoryId == widget.product.category.index),
-              ),
-            ),
-          ],
-        );
-      }),
+            ],
+          );
+        }),
+      ),
     );
   }
 }
